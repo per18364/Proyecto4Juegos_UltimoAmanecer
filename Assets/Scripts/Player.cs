4889bd7500e2512af0f11fc7ace7a4425a3ff8b2
Jorge Pérez <perezjosejorge2013@gmail.com>
2024-05-17 18:18:36 -0600

    Arreglo Ataque Zombies, Manejo de vida y pantalla de game Over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-// using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +8,18 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int hp = 30;
     [SerializeField] private Canvas gameCanvas;
+    [SerializeField] private float damageResetTime = 3.0f;
     private GameObject dmg;
+    private float lastDamageTime; // Tiempo del último golpe recibido
+    private bool isResettingDamage;
+    private int currHp;
     // Start is called before the first frame update
     void Start()
     {
         dmg = gameCanvas.transform.Find("Damage").gameObject;
+        lastDamageTime = Time.time;
+        isResettingDamage = false;
+        currHp = hp;
     }
 
     // Update is called once per frame
@@ -20,12 +27,21 @@ public class Player : MonoBehaviour
     {
         if (dmg.activeSelf)
         {
-            Color dmgColor = dmg.GetComponent<Image>().color;
-            dmgColor.a -= 0.2f * Time.deltaTime;
-            dmg.GetComponent<Image>().color = dmgColor;
-            if (dmgColor.a <= 0)
+            if (isResettingDamage)
             {
-                dmg.SetActive(false);
+                Color dmgColor = dmg.GetComponent<Image>().color;
+                dmgColor.a -= 0.2f * Time.deltaTime;
+                dmg.GetComponent<Image>().color = dmgColor;
+                if (dmgColor.a <= 0)
+                {
+                    dmg.SetActive(false);
+                    isResettingDamage = false;
+                }
+            }
+            else if (Time.time - lastDamageTime >= damageResetTime)
+            {
+                isResettingDamage = true;
+                currHp = hp;
             }
         }
     }
@@ -33,17 +49,21 @@ public class Player : MonoBehaviour
     public void TakeDamage(int damage)
     {
         Debug.Log("Player took " + damage + " damage");
-        hp -= damage;
+        currHp -= damage;
+        lastDamageTime = Time.time;
+        isResettingDamage = false;
 
         // make the damage color appear gradually
         dmg.SetActive(true);
         Color dmgColor = dmg.GetComponent<Image>().color;
         dmgColor.a += 0.33f;
         dmg.GetComponent<Image>().color = dmgColor;
-        if (hp <= 0)
+        if (currHp <= 0)
         {
             Debug.Log("Player is dead");
+            GameObject roundsText = gameCanvas.transform.Find("Roundtext").gameObject;
             GameObject gameOverText = gameCanvas.transform.Find("GameOverText").gameObject;
+            gameOverText.GetComponent<TextMeshProUGUI>().text = "GAME OVER\n\nSobreviviste hasta " + roundsText.GetComponent<TextMeshProUGUI>().text;
             gameOverText.SetActive(true);
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.None;
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -9,6 +9,7 @@ public class Zombie : MonoBehaviour
     private Animation anim;
     private NavMeshAgent agent;
     private Transform player;
+    private bool isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,37 @@ public class Zombie : MonoBehaviour
     }
 
     // Update is called once per frame
+    // void Update()
+    // {
+    //     agent.SetDestination(player.position);
+    // }
+
+    // public void TakeDamage(int damage)
+    // {
+    //     hp -= damage;
+    //     if (hp <= 0)
+    //     {
+    //         agent.SetDestination(transform.position);
+    //         anim.Play("Death");
+    //         Destroy(gameObject, 1f);
+    //     }
+    // }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         anim.Play("Attack1");
+    //         other.GetComponent<Player>().TakeDamage(10);
+    //     }
+    // }
+
     void Update()
     {
-        agent.SetDestination(player.position);
+        if (!isAttacking)
+        {
+            agent.SetDestination(player.position);
+        }
     }
 
     public void TakeDamage(int damage)
@@ -37,10 +66,26 @@ public class Zombie : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isAttacking)
         {
-            anim.Play("Attack1");
-            other.GetComponent<Player>().TakeDamage(10);
+            StartCoroutine(AttackPlayer(other));
         }
+    }
+
+    private IEnumerator AttackPlayer(Collider playerCollider)
+    {
+        isAttacking = true;
+        agent.SetDestination(transform.position); // Detiene al zombie para atacar
+        anim.Play("Attack1");
+        playerCollider.GetComponent<Player>().TakeDamage(10);
+
+        // Espera a que termine la animación de ataque
+        while (anim.IsPlaying("Attack1"))
+        {
+            yield return null;
+        }
+
+        isAttacking = false;
+        anim.Play("Run");
     }
 }
